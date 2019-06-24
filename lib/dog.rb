19c9e67d995e7fae640fde8a097456d7a049f604
Brittany Javalera <brittjavalera@gmail.com>
2019-06-24 02:52:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,18 +2,16 @@ class Dog
   def initialize(name, breed)
       @name = name
       @breed = breed
+      if breed == nil 
+        puts "Mutt"
   end
   
   def name=(name)
     @name = name
   end
   def breed=(breed)
-    if breed == nil
-      puts "Mutt"
-    else
     @breed = breed
   end
-end
   def name
     @name
   end
